e607b2b9051caaff0d17b32f3ae53e3e63de9949
yasharz <y.zeighami@gmail.com>
2025-02-13 14:46:31 -0500

    navbar updated again

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,13 @@
 document.write(`
-    <nav class="navbar">
-        <a href="index.html">Home</a>
-        <a href="team.html">Team</a>
-        <a href="research.html">Research</a>
-        <a href="publications.html">Publications</a>
-        <a href="resources.html">Resources</a>
-    </nav>
+        <nav class="navbar">
+            <a href="index.html" class="logo">AgingLab</a>
+            <div class="hamburger" onclick="toggleMenu()">☰</div>
+            <ul class="nav-links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="team.html">Team</a></li>
+                <li><a href="research.html">Research</a></li>
+                <li><a href="publications.html">Publications</a></li>
+                <li><a href="resources.html">Resources</a></li>
+            </ul>
+        </nav>
     `);
